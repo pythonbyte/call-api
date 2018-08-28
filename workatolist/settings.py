cca58959670ddfdef9e02c09ce02d14b19985d0f
Eduardo <dudumelgaco@hotmail.com>
2018-08-28 18:29:36 -0300

    Change root folder to static

--- workatolist/settings.py
+++ workatolist/settings.py
@@ -123,5 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_root")
+STATIC_ROOT = 'static' # Important for Heroku
 
+STATICFILES_DIRS = (
+    path.join(BASE_DIR, 'static'),  # Important for Heroku
+)
